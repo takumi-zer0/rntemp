e7b350739f59c505ca7a1bedfcc8a6e94f0b77c5
tueno <takumiueno@Takumis-MacBook-Air.local>
2022-03-15 00:11:32 +0900

    init

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import tw from 'twrnc'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={tw`bg-blue-100`}>Open up App.js to start working on your app!</Text>
+    </SafeAreaView>
   );
 }
 
